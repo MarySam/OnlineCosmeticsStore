8b49b91fd72eded5a6c372da36be5ee06ee753e1
Mary Sam <marysam01@hotmail.com>
2015-08-30 22:07:54 -0700

    Added MakeupType (enum)

---
+++ OnlineCosmeticsStore/OnlineCosmeticsStore/Cosmetics.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineCosmeticsStore
+{
+    class Cosmetics
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public string Color { get; set; }
+        public string Brand { get; set; }
+        public MakeupType Type { get; set; }
+
+        public enum MakeupType
+        { Foundation,
+        Mascara,
+        Eyeshadow,
+        Lipstick,
+        NailPolish,
+        Eyeliner,
+        Blush,
+        }
+
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
