73df07619b34cedf461d665b7963f80420087552
Mary Sam <marysam01@hotmail.com>
2015-09-05 15:15:43 -0700

    Welcome and Else/If

--- OnlineCosmeticsStore/OnlineCosmeticsStore/Cosmetics.cs
+++ OnlineCosmeticsStore/OnlineCosmeticsStore/Cosmetics.cs
@@ -33,6 +33,23 @@ namespace OnlineCosmeticsStore
 
         static void Main(string[] args)
         {
+            Console.WriteLine("Welcome to Mary's Online Cosmetic Store!\nAre you interested in seeing what we have in store? Yes or No?");
+            string userValue = Console.ReadLine();
+
+            string message = "";
+
+            if (userValue.ToLower() == "yes")
+            message = "Ok, great!";
+
+            else if (userValue.ToLower() == "no")
+            message = "Ok, have a great day!";
+
+            else
+                message = "Please restart the program and type in 'Yes' or 'No'";
+
+            Console.WriteLine(message);
+            Console.ReadLine();
+
         }
     }
 }
