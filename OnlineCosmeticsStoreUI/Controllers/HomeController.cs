98934ac0bae194559a4af27691942fd49802dd66
Mary Sam <marysam01@hotmail.com>
2015-11-22 23:44:52 -0800

    Web front end and viewing customer account.

--- OnlineCosmeticsStoreUI/Controllers/HomeController.cs
+++ OnlineCosmeticsStoreUI/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using OnlineCosmeticsStore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,8 +9,25 @@ namespace OnlineCosmeticsStoreUI.Controllers
 {
     public class HomeController : Controller
     {
+        //This is the default method.  The Index is the entry point similar to the main method. 
         public ActionResult Index()
         {
+            //This is the html front end.  What displays to the user.
+            return View();
+        }
+        //The ActionResult is a return TYPE. 
+        //SignIn is the method name.
+        //FormCollection collection is the parameter.
+        public ActionResult SignIn(FormCollection collection)
+        {
+            //This is going to call the index.cshtml and find the textbox called "txtEmail"
+            var email = collection["txtEmail"];
+            {
+                var accounts = CustomerInformation.GetAllCustomerInformationByEmail(email);
+                //Prints out the all accounts in the html.
+                return View(accounts);
+            }
+
             return View();
         }
 
